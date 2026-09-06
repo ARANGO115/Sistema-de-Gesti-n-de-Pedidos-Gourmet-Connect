7c54f182797d086744588740787f1b89a1205220
DeiviDiaz <deividiazg5@gmail.com>
2026-09-06 11:56:38 -0500

    feat: preparar autenticacion de usuarios

--- proyect-gourmet-connect/src/app.module.ts
+++ proyect-gourmet-connect/src/app.module.ts
@@ -7,6 +7,7 @@ import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { RolesModule } from './roles/roles.module';
     AppConfigModule,
     UsersModule,
     RolesModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
